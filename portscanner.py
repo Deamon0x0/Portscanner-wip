401f487d6d373e5e4902213466259ecbda19e693
0xDaemon <155391588+Deamon0x0@users.noreply.github.com>
2024-06-04 17:51:42 +0100

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,83 @@
+##### imports libararies ######
+import socket
+from queue import Queue
+from colorama import Fore, init
+import threading
+import argparse
+
+
+########################## Portscanner Python  #####################
+
+
+
+ascii_art = '''
+        
+ ███████ ██ ███    ███ ██████  ██      ███████               
+██      ██ ████  ████ ██   ██ ██      ██                    
+███████ ██ ██ ████ ██ ██████  ██      █████                 
+     ██ ██ ██  ██  ██ ██      ██      ██                    
+███████ ██ ██      ██ ██      ███████ ███████               
+                                                            
+                                                            
+██████   ██████  ██████  ████████                           
+██   ██ ██    ██ ██   ██    ██                              
+██████  ██    ██ ██████     ██                              
+██      ██    ██ ██   ██    ██                              
+██       ██████  ██   ██    ██                              
+                                                            
+                                                            
+███████  ██████  █████  ███    ██ ███    ██ ███████ ██████  
+██      ██      ██   ██ ████   ██ ████   ██ ██      ██   ██ 
+███████ ██      ███████ ██ ██  ██ ██ ██  ██ █████   ██████  
+     ██ ██      ██   ██ ██  ██ ██ ██  ██ ██ ██      ██   ██ 
+███████  ██████ ██   ██ ██   ████ ██   ████ ███████ ██   ██ 
+                                                            
+                                                        
+'''
+
+####################################### declear colors #######################
+
+init()
+
+RED = Fore.RED
+GREEN = Fore.GREEN
+BLUE = Fore.BLUE
+RESET = Fore.RESET
+
+
+print(f"{BLUE} {ascii_art} {RESET} ")
+
+####################################### Setup connections ####################
+
+def Scan(host, port):
+    #setup connection with socket
+    connection = socket.socket()
+    try:
+        #connecting to host
+        connection.connect((host, port))
+    except:
+        #connect Failed
+        return False
+    else:
+        #connection Success
+        return True
+
+
+################ receive input ###############
+
+host = input("Enter Host to scan: ")
+ports = int(input("Enter numbers of ports to scan: "))
+
+################## loop and scan ports ##########
+
+for port in range(1, ports):
+    #### if port is open bool=true
+    if Scan(host, port):
+        print(f"{GREEN} [+] {host}: port {port} is open {RESET} \r")
+    else:
+        print(f"{RED} [!] {host}: port {port} is closed/filtered! {RESET} \r")
+
+
+################################ using argument parser to add argument ########################
+
+
